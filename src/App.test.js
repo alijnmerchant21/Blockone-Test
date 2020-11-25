0af5695f78d337463be865013f86618eadbaf0ab
Aliasgar Merchant <44069404+alijnmerchant21@users.noreply.github.com>
2020-11-25 22:34:27 +0530

    Fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Load Data Button Text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/load data/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Blocks Text', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/blocks/i);
   expect(linkElement).toBeInTheDocument();
 });
